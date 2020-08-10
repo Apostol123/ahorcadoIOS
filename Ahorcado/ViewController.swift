5b39fadbc568a07b8060e55265ec71c0ea630b0e
Apostol123 <apostol516@gmail.com>
2020-08-10 07:56:10 +0100

    first commit

---
+++ Ahorcado/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ahorcado
+//
+//  Created by user159106 on 8/10/20.
+//  Copyright © 2020 user159106. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
