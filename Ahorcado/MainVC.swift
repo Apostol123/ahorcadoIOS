6520592b980f871d5b9c05ccd27a8904c5659611
Apostol123 <apostol516@gmail.com>
2020-08-10 08:03:13 +0100

    create mainVC and remove storyBoards

---
+++ Ahorcado/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainVC.swift
+//  Ahorcado
+//
+//  Created by user159106 on 8/10/20.
+//  Copyright © 2020 user159106. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
